e664494f866722446288c5ffced389d479ab4337
disjfjdizmfnkf <3037650079@qq.com>
2024-04-19 16:46:52 +0800

    å完成动态æ详情详情评论条数和列表的返回

--- src/service/moment.service.js
+++ src/service/moment.service.js
@@ -8,18 +8,19 @@ class MomentService {
         return result
     }
 
+    // 查询动态返回评论个数
     async queryList(offset = 0, size = 10) {
         const statement =
             `
-                SELECT m.user_id                                                                               id,
+                SELECT m.id                                                                                    id,
                        m.content                                                                               content,
                        u.name                                                                                  name,
                        m.updateAt                                                                              updateTime,
                        m.createAt                                                                              createTime,
-                       JSON_OBJECT('id', u.id, 'name', u.name, 'createAt', u.createAt, 'updateAt', m.updateAt) user
+                       JSON_OBJECT('id', u.id, 'name', u.name, 'createAt', u.createAt, 'updateAt', m.updateAt) user,
+                       (SELECT COUNT(*) FROM comment WHERE comment.moment_id = m.id)                           commentCount
                 FROM \`moment\` m
-                         LEFT JOIN user u
-                                   ON m.user_id = u.id
+                         LEFT JOIN user u ON m.user_id = u.id
                 LIMIT ? OFFSET ?;
             `
 
@@ -27,20 +28,23 @@ class MomentService {
         return result
     }
 
-
+    // 查询动态返回评论列表
     async queryByID(id) {
         const statement =
             `
-                SELECT m.user_id                                                                               id,
-                       m.content                                                                               content,
-                       u.name                                                                                  name,
-                       m.updateAt                                                                              updateTime,
-                       m.createAt                                                                              createTime,
-                       JSON_OBJECT('id', u.id, 'name', u.name, 'createAt', u.createAt, 'updateAt', m.updateAt) user
-                FROM \`moment\` m
-                         LEFT JOIN user u
-                                   ON m.user_id = u.id
-                WHERE m.id = ?;
+                SELECT m.id id, m.content content, m.createAt creatTime, m.updateAt updateTime,
+                       JSON_OBJECT('id', u.id, 'name', u.name, 'creatTime', u.createAt ) user,
+                       (
+                           JSON_ARRAYAGG(JSON_OBJECT('id', c.id, 'content', c.content, 'commentId', c.comment_id,
+                                                     'user', JSON_OBJECT('id', cu.id, 'name', cu.name))
+                           )
+                           )
+                FROM moment m
+                         LEFT JOIN user u ON u.id = m.user_id
+                         LEFT JOIN comment c on m.id = c.moment_id
+                         LEFT JOIN user cu on c.user_id = cu.id
+                WHERE m.id = ?
+                GROUP BY m.id;
             `
 
         const [result] = await connection.execute(statement, [id])
