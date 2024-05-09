d58e08032dd7d96b445889717c6146b6bb44d40f
disjfjdizmfnkf <3037650079@qq.com>
2024-05-09 22:16:08 +0800

    给llm添加prompt

--- src/config/server.js
+++ src/config/server.js
@@ -3,6 +3,7 @@ const dotenv = require('dotenv')
 dotenv.config()
 
 module.exports = {
+    PROMPT,
     API_KEY,
     SECRET_KEY,
     SERVER_PORT,
--- src/controller/chatGpt.controller.js
+++ src/controller/chatGpt.controller.js
@@ -1,5 +1,5 @@
 const axios = require('axios');
-const { API_KEY, SECRET_KEY} = require('../config/server')
+const { API_KEY, SECRET_KEY, PROMPT} = require('../config/server')
 
 
 const getAccessToken = async () => {
@@ -17,8 +17,11 @@ const askToChatGpt = async function (ctx) {
                     content: ctx.request.body.message,
                 },
             ],
+            system: PROMPT, //使用自己定义的prompt
+            // stream: true, //以流式接口的形式返回数据
         });
         // 响应体内容 koa会自动设置响应头
+        console.log(resp)  //看下开发api调用到期没，每天次数够不够。。。
         ctx.body = { from: "chatGpt", data: resp.data.result };
     } catch (error) {
         console.log("Error ", error);
